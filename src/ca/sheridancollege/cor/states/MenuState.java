a90961005f49087be3cd3425db3f261f23a0d6bf
mellowboyXD <chamerallyishfaq@gmail.com>
2026-03-10 13:05:41 -0400

    Print menu after viewing the deck

--- src/ca/sheridancollege/cor/states/MenuState.java
+++ src/ca/sheridancollege/cor/states/MenuState.java
@@ -12,75 +12,81 @@ import java.util.Scanner;
  * TODO: Should render menu and process input.
  * TODO: Implement separate view for rendering
  * TODO: Implement InputController for input handling
+ * 
  * @author mellowboy
  */
-public class MenuState implements GameState{
+public class MenuState implements GameState {
 
-	private final GameData data;
-	private final Scanner scanner;
-	private boolean startGame;
+    private final GameData data;
+    private final Scanner scanner;
+    private boolean startGame;
 
-	public MenuState(GameData data) {
-		this.data = data;
-		this.scanner = data.getScanner();
-		startGame = false;
-	}
+    public MenuState(GameData data) {
+        this.data = data;
+        this.scanner = data.getScanner();
+        startGame = false;
+    }
 
-	@Override
-	public void enter() {
-		System.out.println("==== CARDS OF RUIN ====");
-		System.out.println("1. Play/Continue");
-		System.out.println("2. View Deck");
-		System.out.println("3. Exit");
-	}
+    private void printMenu() {
+        System.out.println("==== CARDS OF RUIN ====");
+        System.out.println("1. Play/Continue");
+        System.out.println("2. View Deck");
+        System.out.println("3. Exit");
+    }
 
-	@Override
-	public void update() {
-		int choice = -1;
-		do {
-			System.out.print("> ");
-			try {
-				choice = scanner.nextInt();
-				switch (choice) {
-					case 1 -> startGame = true;
-					case 2 -> showDeck();
-					case 3 -> System.exit(0);
-					default -> throw new IllegalStateException("Invalid Option");
-				}
-			} catch (InputMismatchException ex) {
-				System.out.println("Invalid input. Enter integers only.");
-				scanner.next();
-				choice = -1;
-			} catch (IllegalStateException ex) {
-				System.out.println("Invalid Input. Choose between 1 to 3.");
-				choice = -1;
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		} while (choice == -1);
-	}
+    @Override
+    public void enter() {
+        printMenu();
+    }
 
-	@Override
-	public void exit() {
-		System.out.println("\n==== SETUP PHASE ====");
-	}
+    @Override
+    public void update() {
+        int choice = -1;
+        do {
+            System.out.print("> ");
+            try {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1 -> startGame = true;
+                    case 2 -> showDeck();
+                    case 3 -> System.exit(0);
+                    default -> throw new IllegalStateException("Invalid Option");
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Invalid input. Enter integers only.");
+                scanner.next();
+                choice = -1;
+            } catch (IllegalStateException ex) {
+                System.out.println("Invalid Input. Choose between 1 to 3.");
+                choice = -1;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } while (choice == -1);
+    }
 
-	@Override
-	public GameState nextState() {
-		if (startGame)
-			return new SetupState(data);
+    @Override
+    public void exit() {
+        System.out.println("\n==== SETUP PHASE ====");
+    }
 
-		// stay in current state
-		return null;
-	}
+    @Override
+    public GameState nextState() {
+        if (startGame)
+            return new SetupState(data);
 
-	private void showDeck() {
-		System.out.println("\n==== Deck ====");
-		var deck = data.getDeck();
-		if (deck != null) {
-			for (Card c : deck.getCards()) {
-				System.out.println(c);
-			}
-		}
-	}
+        // stay in current state
+        return null;
+    }
+
+    private void showDeck() {
+        System.out.println("\n==== Deck ====");
+        var deck = data.getDeck();
+        if (deck != null) {
+            for (Card c : deck.getCards()) {
+                System.out.println(c);
+            }
+            printMenu();
+        }
+    }
 }
