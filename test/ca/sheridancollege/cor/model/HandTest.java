3e8f123eef97a8e58b8fa6dd5c9d56202a424d84
blackpanther9090 <hassen.ibrahim1@gmail.com>
2026-03-30 20:06:38 -0400

    Modified the project for test purposes for HandTest

---
+++ test/ca/sheridancollege/cor/model/HandTest.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package ca.sheridancollege.cor.model;
+
+import ca.sheridancollege.cor.model.cards.Card;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hassenibrahim
+ */
+public class HandTest {
+    
+    public HandTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of draw method, of class Hand.
+     */
+    @Test
+    public void testDraw() {
+        System.out.println("draw");
+        Hand instance = new Hand(5);
+        instance.setup();
+        int sizeBefore = instance.getCards().size();
+        Card result = instance.draw();
+        assertNotNull(result);
+        assertEquals(sizeBefore - 1, instance.getCards().size());
+        
+    }
+    
+}
