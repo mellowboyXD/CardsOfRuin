231a435b44450d5c9dec3670951178243aeef29e
mellowboyxd <chamerallyishfaq@gmail.com>
2026-04-03 12:29:38 -0400

    refactor: refactored code and correct some mistakes

--- src/ca/sheridancollege/cor/model/Monster.java
+++ src/ca/sheridancollege/cor/model/Monster.java
@@ -1,7 +1,8 @@
 package ca.sheridancollege.cor.model;
 
 /**
- *
+ * This is the monster class, and it uses a builder design pattern to create a new monster.
+ * It allows the class to be flexible and monster creation do now have to be bound to constructors.
  * @author mellowboy
  */
 public class Monster extends Entity {
@@ -15,18 +16,17 @@ public class Monster extends Entity {
 	/**
 	 * Use this method to set up the monster when the game initially starts or restarts.
 	 * Prefer to use builder patterns when adjusting monsters between rounds.
+	 * This method does nothing but is required as Monster needs to implement it.
 	 */
 	@Override
 	public void setup() {
-		super.setHealth(100);
-		super.setAttack(20);
-		super.setShield(50);
 	}
 
 	/**
-	 * This is an inner class that is responsible to build the monster.
-	 * This contains the base for the builder pattern.
-	 */
+     * This is an inner class that is responsible to build the monster.
+     * This contains the base for the builder pattern.
+     * I took inspiration from this <a href="https://www.baeldung.com/java-builder-pattern">blog post</a>
+     */
 	public static class Builder {
 		private int health;
 		private int shield;
