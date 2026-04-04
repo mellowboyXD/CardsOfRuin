bc2e63987d82f6cc165b58f5c54cd3020bb5123c
mellowboyxd <chamerallyishfaq@gmail.com>
2026-04-04 08:29:33 -0400

    refactor: removed unnecessary lines

--- test/ca/sheridancollege/cor/controller/GameControllerTest.java
+++ test/ca/sheridancollege/cor/controller/GameControllerTest.java
@@ -1,40 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package ca.sheridancollege.cor.controller;
 
 import ca.sheridancollege.cor.states.GameContext;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- *
+ * This class tests the main GameController class.
  * @author hassenibrahim
+ * @author mellowboyxd
  */
 public class GameControllerTest {
-    
+
+    private GameController instance;
+
     public GameControllerTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        instance = new GameController("Testing GameController class");
+        instance.setup();
     }
 
     /**
@@ -43,9 +30,6 @@ public class GameControllerTest {
     @Test
     public void testSetup() {
         System.out.println("setup");
-        GameController instance = new GameController("TestGame");
-        instance.setup();
-        // TODO review the generated test code and remove the default call to fail.
         assertNotNull(instance.getContext());
         assertNotNull(instance.getContext().getState());
     }
@@ -56,31 +40,10 @@ public class GameControllerTest {
     @Test
     public void testGetContext() {
         System.out.println("getContext");
-        GameController instance = new GameController("TestGame");
-
         GameContext result = instance.getContext();
-
-    // Context should be initialized, not null
+        // Context should be initialized, not null
         assertNotNull(result);
-
-    // The context should have a state (MenuState) after setup
+        // The context should have a state (MenuState) after setup
         assertNotNull(result.getState());
-
     }
-
-    /**
-     * Test of getTitle method, of class GameController.
-     */
-    @Test
-    public void testGetTitle() {
-        System.out.println("getTitle");
-        GameController instance = new GameController("MyCardGame");
-
-        String result = instance.getTitle();
-
-        assertEquals("MyCardGame", result);
-    }
-
-    
-    
 }
